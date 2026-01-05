cd5e77cd296e26a36f867f268b2343646f399097
isa851 <isa904363@gmail.com>
2026-01-05 16:47:06 +0600

    fix

--- handlers/keyboard.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-# Создаём клавиатуру с одной кнопкой /start
-main_keyboard = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="/start")] 
-    ],
-    resize_keyboard=True 
-)
